c346b39150b74bb35f5fabb74003c60d61ca5863
Andrea Reichle <andrea_reichle@yahoo.com>
2019-09-25 18:07:00 -0500

    created add.html, displayAddItemForm, and processAddItemForm

--- src/main/java/org/launchcode/resaleshopinventory/controllers/ItemController.java
+++ src/main/java/org/launchcode/resaleshopinventory/controllers/ItemController.java
@@ -1,15 +1,44 @@
 package org.launchcode.resaleshopinventory.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 @Controller
+@RequestMapping("item")
 public class ItemController {
 
+    static ArrayList<String> items = new ArrayList<>();
+
+    // Request path: /item
     @RequestMapping(value = "")
-    @ResponseBody
-    public String index() {
-        return "My first item";
+    public String index(Model model) {
+
+        model.addAttribute("items", items);
+        model.addAttribute("title", "My Items");
+        return "item/index";
     }
+
+    // Request path: item/add
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddItemForm(Model model) {
+
+        model.addAttribute("title", "Add Item");
+        return "item/add";
+    }
+
+    // Request path: item/add
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddItemForm(@RequestParam String itemName) {
+
+        items.add(itemName);
+
+        //Redirect to item/
+        return "redirect:";
+    }
+
 }
